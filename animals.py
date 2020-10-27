bbf9122df550acbc8d1d0327dc5d46449b8db270
olamide oladinni <olajide1987@gmail.com>
2020-10-27 18:42:34 -0500

    chpt 9 complete

---
+++ animals.py
@@ -0,0 +1,24 @@
+from catfish import Catfish
+from chicken import Chicken
+from anaconda import Anaconda
+from coralsnake import CoralSnake
+from mallard import Mallard
+from blackmamba import BlackMamba
+from lamb import Lamb
+from egret import Egret
+from bronco import Bronc
+from camel import camel
+from kingsnake import KingSnake
+from blackswan import BlackSwan
+from frog import Frog
+from watersnake import WaterSnake
+
+
+
+def main():
+    myCatFish = Catfish("Wall Singer", "Chicken of the Sea")
+
+    print(myCatFish)
+
+
+main()
